d2ac4baf3eb4ab479edc4c8ecbbe907ce833832e
sharadParadhi <sharadparadhi2124@gmail.com>
2025-10-05 13:56:53 +0530

    changed backend deployed url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://quizz-app-ijaf.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
